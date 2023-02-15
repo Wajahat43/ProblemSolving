2a853e8c751ffcb9afd56c7492b44b3b13bddf05
Wajahat Islam Gul <69782348+Wajahat43@users.noreply.github.com>
2023-02-15 17:39:34 +0500

    Time: 52 ms (62.40%), Space: 19 MB (10.28%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -5,10 +5,14 @@ public:
             return 0;
 
         long long int max = piles[0];
+        //find the max element in array our answer cannot be greater than this max
         for(int i=0;i<piles.size();i++){
             if(piles[i] > max)
                 max = piles[i];
         }
+        
+        //start from 1 to max and do binary search to find the lowest number that satisfies
+        //the constraint.
         long long int low = 1;
         long int high = max;
         long long int solSoFar = INT_MAX;
