689b3279ddee0910d9f904b958d4fd2fbb566a47
Wajahat Islam Gul <69782348+Wajahat43@users.noreply.github.com>
2023-05-17 19:09:19 +0500

    Time: 282 ms (16.36%), Space: 121.2 MB (41.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,15 +11,17 @@
 class Solution {
 public:
     bool isPalindrome(ListNode** first, ListNode* last){
+        //if we have reached end of list return true
         if(!last)
             return true;
+        //get result by moving last to next
         bool result = isPalindrome(first,last->next);
+        //compare result and also the values of first and last node in current function call
         if((*first)->val == last->val && result){
             (*first) = (*first)->next;
             return true;
         }
             
-        
         return false;
     }
     bool isPalindrome(ListNode* head) {
