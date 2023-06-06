387183e361ac7c1e16849460482fa60c95aaee3b
Wajahat Islam Gul <69782348+Wajahat43@users.noreply.github.com>
2023-06-06 12:07:01 +0500

    Time: 1169 ms (8.16%), Space: 16.8 MB (5.32%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
@@ -4,8 +4,12 @@ class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         n = len(stations)
         dp = [startFuel] + [0] * n
+        #dp[i] is maximum distance we can reach by refueling at i stations
         for i in range(n):
+            #for all the count of previous stations, check if we could reach current station
             for j in range(i, -1, -1):
+                #if we could reach current station with j reufeling stops, update max distance of j+1 
+                #to j+1's current value or dp[j] + currentStationsFuel.
                 if dp[j] >= stations[i][0]:
                     dp[j+1] = max(dp[j+1], dp[j] + stations[i][1])
         for i in range(n+1):
