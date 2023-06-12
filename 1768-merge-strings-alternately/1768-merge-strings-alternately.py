98c358c994428f8292a71980525627799cdd7c25
Wajahat Islam Gul <69782348+Wajahat43@users.noreply.github.com>
2023-06-12 19:45:46 +0500

    Time: 57 ms (5.96%), Space: 16.4 MB (13.19%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,10 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         res = ""
+        #iterate over the stirngs and add their characters until one or both of them runout
         for f, s in zip(word1,word2):
             res += f + s
+        #if first string is greater then second, add the rest of it to res
         if len(word1) > len(word2):
             res += word1[len(word2):]
+        #if second string is greater than first, add the rest of it to res
         elif len(word2) > len(word1):
             res += word2[len(word1):]
         return res
