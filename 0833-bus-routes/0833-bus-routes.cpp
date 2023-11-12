c46d1dba15ce06ec56274d61bbaebec8914fbd2e
Wajahat Islam Gul <69782348+Wajahat43@users.noreply.github.com>
2023-11-12 15:29:32 +0500

    Time: 710 ms (29.72%), Space: 55.5 MB (90.36%) - LeetHub

--- 0833-bus-routes/0833-bus-routes.cpp
+++ 0833-bus-routes/0833-bus-routes.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    /*
+    1. Each values in routes is the number of stops ith bus takes. (This is like strongly connected components of a bus).
+    2. We can form an adjList of bus stops and use BFS on each stop to reach target. But as there can be 500 buses and each bus can have 10000 stops this increases the search space too much.
+    3. Instead for each stop of bus we include the bus number (route number) in its adjList. We can reach all the other stops using this bus.
+    4. Instead of running bfs on stops we run it on routes in other words buses instead of stops. As there can be only 500 buses(routes) we can this reduces the search space.
+    */
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         if (source == target) {
             return 0;
