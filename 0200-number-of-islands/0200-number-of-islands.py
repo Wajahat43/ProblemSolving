1bae959057594034f5a48ed4249891caed96d76e
Wajahat Islam Gul <69782348+Wajahat43@users.noreply.github.com>
2024-01-24 09:22:54 +0500

    Time: 255 ms (72.18%), Space: 19 MB (83.38%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        def bfs(i,j):
+            q = Deque()
+            q.append((i,j))
+            neighbors = [[1,0], [0,1], [-1,0], [0,-1]]
+            marked = set()
+            while q:
+                celli, cellj = q.popleft()
+                grid[celli][cellj] = "0"
+                
+                for ni, nj in neighbors:
+                    newi, newj = celli+ni, cellj+nj
+                    if newi >= 0 and newi < len(grid) and newj >= 0 and newj < len(grid[i]) and (newi, newj) not in marked and grid[newi][newj] == "1":
+                        q.append((newi,newj))
+                        marked.add((newi,newj))
+        
+        result = 0
+        for i,row in enumerate(grid):
+            for j,cell in enumerate(row):
+                
+                if cell == "1":
+                    
+                    bfs(i,j)
+                    result += 1
+                    print(result, end = " ")
+        return result
+        
